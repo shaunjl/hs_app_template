02416129f502cbe99364c29397202a701666f065
Shaun <shauntheta@gmail.com>
2014-03-26 16:09:55 -0600

    4/26/14

--- models.py
+++ models.py
@@ -6,17 +6,18 @@ from mezzanine.core.models import Ownable
 from hs_core.models import AbstractResource
 
 class timeSeries(Page, RichText,AbstractResource):
-	resource_description = models.TextField(null=False, blank=True, default='',
-                help_text='I.E. Time Series',
-        )
-	resource_url=models.TextField(max_length=100,null=False, blank=True, default='',
-		help_text="The WSDL Url for your web services- I.E. 'http://worldwater.byu.edu/interactive/dr/services/index.php/services/cuahsi_1_1.asmx?WSDL'"
+    resource_description = models.TextField(null=False, blank=True, default='',
+        help_text='I.E. Upper Provo River Flow',
+    )
+    resource_url=models.TextField(max_length=100,null=False, blank=True, default='',
+        help_text="The WSDL Url for your web services- I.E. 'http://worldwater.byu.edu/interactive/dr/services/index.php/services/cuahsi_1_1.asmx?WSDL'"
+    )
+    resource_namespace=models.TextField(max_length=100,null=False, blank=True, default='',
+        help_text='The namespace is probably {http://www.cuahsi.org/waterML/1.1/}'
 	)
-	resource_namespace=models.TextField(max_length=100,null=False, blank=True, default='',
-		help_text='The namespace is probably {http://www.cuahsi.org/waterML/1.1/}'
-	)
-	class Meta:
-		verbose_name = "Time Series"
+
+    class Meta:
+        verbose_name = "Time Series"
 
 
 
